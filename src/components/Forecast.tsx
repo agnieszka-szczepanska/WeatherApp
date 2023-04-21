42b3282112037cf34725fb0afad931c6f6a11351
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-04-21 21:42:20 +0200

    display city name and temperature

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -5,9 +5,20 @@ type Props = {
 }
 
 const Forecast = ({ data }: Props): JSX.Element => {
+  const today = data.list[0]
   return (
-    <div>
-      <p></p>
+    <div className="w-full md:max-w-[500px] py md:py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shawod-lg">
+      <div className="mx-auto w-[300px]">
+        <section className="text-center">
+          <h2 className="text-2xl font-black">
+            {data.name} <span className="font-thin">{data.country}</span>
+          </h2>
+          <h1 className="text-4xl font-extrabold">
+            {Math.round(today.main.temp)}
+            <sup>o</sup>
+          </h1>
+        </section>
+      </div>
     </div>
   )
 }
