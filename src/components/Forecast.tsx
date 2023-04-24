20ef29f1583fd7d8a0a7160bd03560771ea6d118
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-04-24 22:09:38 +0200

    displays pressure tile

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -98,6 +98,14 @@ const Forecast = ({ data }: Props): JSX.Element => {
             info={`${Math.round(today.pop * 100)}%`}
             description={`${getPop(today.pop)}, clouds at ${today.clouds.all}%`}
           />
+          <Tile
+            icon="pressure"
+            title="Pressure"
+            info={`${today.main.pressure} hPa`}
+            description={` ${
+              Math.round(today.main.pressure) < 1013 ? 'Lower' : 'Higher'
+            } than standard`}
+          />
         </section>
       </div>
     </div>
